62009f639d2f5d3a1b7910c80792d73804a13a1e
goiaba <affsrodie@gmail.com>
2022-04-08 13:47:32 -0500

    cursor

--- start.sh
+++ start.sh
@@ -3,6 +3,12 @@ yay -S notion-app insync insync-dolphin masterpdfeditor-free visual-studio-code-
 
 sudo pacman -S --needed papirus-folders papirus-icon-theme krita xournalpp libreoffice-fresh-pt-br
 
-papirus-folders -C teal  
+papirus-folders -C teal
+
+tar -zxf material_cursors.tar.gz -C /usr/share/icons/
+
+sudo sed -i 's/Inherits=.*/Inherits=material_cursors/' /usr/share/icons/default/index.theme
 
 sh gaomon-config.sh
+
+
