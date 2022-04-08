de7082dfeb031ac04b979106157551716e8d5546
goiaba <affsrodie@gmail.com>
2022-04-08 13:04:22 -0500

    start files

---
+++ gaomon-config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo pacman -S --needed  linux linux-headers xf86-input-wacom dkms base-devel
+
+mkdir  ~/apps
+
+yay -S digimend-kernel-drivers-dkms-git
+
+sudo cp -f huion-tablet.conf /etc/X11/xorg.conf.d/
+
+sudo cp -f 99-wacom.rules /etc/udev/rules.d/
+
+cp -f keys.sh ~/apps/
+
+chmod +x ~/apps/*
+
+mkdir -p ~/.config/systemd/user
+
+cp -f wacom.service ~/.config/systemd/user/wacom.service
+
+systemctl --user enable wacom.service
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yay -S notion-app insync insync-dolphin masterpdfeditor-free visual-studio-code-bin hunspell-pt-br nordic-theme-git kvantum-theme-nordic-git nordic-kde-git
+
+sudo pacman -S --needed papirus-folders papirus-icon-theme krita xournalpp libreoffice-fresh-pt-br
+
+papirus-folders -C teal  
+
+sh gaomon-config.sh
