5a15fe842225d5773d861bed04691c685f0a9930
rodiecko <affsrodie@gmail.com>
2022-06-19 19:54:02 -0500

    update key.sh

--- keys.sh
+++ keys.sh
@@ -21,23 +21,37 @@ xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" Rotate half   #
 
 #para usar igual touch
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "Button" "2" "pan"
-xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "PanScrollThreshold" 350 
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "PanScrollThreshold" 300
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "Threshold" 15
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "PressureRecalibration" off
+
+#Option "Area" "0 0 50800 31750"
+#Option "Suppress" "2"
+#Option "RawSample" "4"
+#Option "PressCurve" "0 0 100 100"
+#Option "Mode" "Absolute"
+#Option "CursorProx" "30"
+#Option "Rotate" "half"
 
 #xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO stylus" "Button" "3" "button +2 " 
 
 
-xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "14" "key m"
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "14" "key +space"
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "13" "key +shift +space"
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "12" "key ="
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "11" "key -"
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "10" "key +ctl z -ctl"
 
 
-xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "9" "button +9"#"voltar para frente" do mouse
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "9" "button +9" #"voltar para frente" do mouse
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "8" "key e"
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "3" "key +ctl s -ctl"
 xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "2" "key +alt tab"
-xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "1" "button +8"#voltar página =="alt <-" "backspace no dolphin"
+xsetwacom --set "GAOMON Gaomon Tablet_M10KPRO Pad pad" "Button" "1" "button +8" #voltar página =="alt <-" "backspace no dolphin"
 
 
 notify-send "Eu executei mai lirou frend"
+
+
+
+
--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@
 mkdir ~/build
 cd ~/build
 
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
+#git clone https://aur.archlinux.org/yay-bin.git
+#cd yay-bin
+#makepkg -si
 
-sudo pacman -S --needed papirus-icon-theme krita xournalpp libreoffice-fresh-pt-br ttf-fira-code
+#sudo pacman -S --needed papirus-icon-theme krita xournalpp libreoffice-fresh-pt-br ttf-fira-code
 
 yay -S ttf-meslo-nerd-font-powerlevel10k papirus-folders-git notion-app insync insync-dolphin masterpdfeditor-free visual-studio-code-bin hunspell-pt-br nordic-theme-git kvantum-theme-nordic-git nordic-kde-git
 
@@ -18,8 +18,8 @@ cd ~/build/arch/
 
 sudo cp -r material_light_cursors /usr/share/icons/
 
-sudo sed -i 's/Inherits=.*/Inherits=material_light_cursors/' /usr/share/icons/default/index.theme
+#sudo sed -i 's/Inherits=.*/Inherits=material_light_cursors/' /usr/share/icons/default/index.theme
 
-sh gaomon-config.sh
+#sh gaomon-config.sh
 
 
