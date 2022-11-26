60ee1a72571e9f6b5d587c74ed71166d4e8a6665
rodie <affsrodie@gmail.com>
2022-11-25 21:39:19 -0500

    mjro

--- start.sh
+++ start.sh
@@ -10,16 +10,17 @@ cd ~/build
 
 #sudo pacman -S --needed papirus-icon-theme krita xournalpp libreoffice-fresh-pt-br ttf-fira-code
 
-yay -S ttf-meslo-nerd-font-powerlevel10k papirus-folders-git notion-app insync insync-dolphin masterpdfeditor-free visual-studio-code-bin hunspell-pt-br nordic-theme-git kvantum-theme-nordic-git nordic-kde-git
+#yay -S ttf-meslo-nerd-font-powerlevel10k papirus-folders-git notion-app insync insync-dolphin masterpdfeditor-free visual-studio-code-bin hunspell-pt-br nordic-theme-git kvantum-theme-nordic-git nordic-kde-git
+yay -S  insync insync-dolphin masterpdfeditor-free hunspell-pt-br nordic-theme-git kvantum-theme-nordic-git nordic-kde-git
 
 papirus-folders -C teal
 
 cd ~/build/arch/
 
-sudo cp -r material_light_cursors /usr/share/icons/
+#sudo cp -r material_light_cursors /usr/share/icons/
 
 #sudo sed -i 's/Inherits=.*/Inherits=material_light_cursors/' /usr/share/icons/default/index.theme
 
-#sh gaomon-config.sh
+sh gaomon-config.sh
 
 
